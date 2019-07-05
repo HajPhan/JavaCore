90787531c2f04c1dba6736fbc998844dd0f4506d
HajPhan <hajphan97@gmail.com>
2019-07-05 10:56:39 +0700

    Upload Project :smile:

---
+++ Java-This-Keyword/src/mta/edu/vn/test/Test.java
@@ -0,0 +1,19 @@
+package mta.edu.vn.test;
+
+/**
+ * This Keyword
+ *
+ * 1. This can be used to refer current class instance variable
+ * 2. This can be used to invoke current class method (implicitly) (ngụ ý)
+ * 3. This() can be used to invoke current class contructor
+ * 4. This can be passed as argument in the method call
+ * 5. This can be passed as an argument in the constructor call
+ * 6. This can be used to return the current class instance from the method
+ *
+ */
+
+public class Test {
+    public static void main(String [] args){
+        System.err.println("Hello...");
+    }
+}
