487664fe8c294fc54d02f72338943f54ffd39698
HajPhan <hajphan97@gmail.com>
2019-07-05 16:20:29 +0700

    Upload new project :smile:

--- Java-This-Keyword/src/mta/edu/vn/test/Test.java
+++ Java-This-Keyword/src/mta/edu/vn/test/Test.java
@@ -15,5 +15,23 @@ package mta.edu.vn.test;
 public class Test {
     public static void main(String [] args){
         System.err.println("Hello...");
+        String str1 = "Welcome";
+        String str2 = "Welcome";
+        System.err.println(str1 == str2);
+
+        String str3 = new String("Hello");
+        String str4 = new String("Hello");
+        String str5 = str1.intern();
+        int check = str3.compareTo(str4);
+        System.out.println("str1 == str3 : " + (str1 == str3));
+        System.out.println("str5.equals(str3) : " + str5.equals(str3));
+        System.err.println("str5 == str3 : " + (str5 == str3));
+        System.err.println("str1.equals(str3) : " + str1.equals(str3));
+        System.out.println("str3 == str4 : " + (str3 == str4));
+        System.out.println("str3.intern() == str4.intern() : " + (str3.intern() == str4.intern()));
+        System.out.println("str3.equals(str4) : " + str3.equals(str4));
+        System.err.println("check = " + check);
+        System.err.println(str1.hashCode() + " :: " + str3.hashCode() + " :: " + str4.hashCode());
+
     }
 }
